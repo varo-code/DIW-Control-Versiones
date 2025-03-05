f665886a4ba8e8f305199112244d9d1b69fcb4af
Varo <alvarodelcanperezbuzymaria@gmail.com>
2025-03-05 03:44:47 +0100

    Add files via upload

---
+++ proyecto-web/apartados-del proyecto.py
@@ -0,0 +1,119 @@
+"""
+    Apartados de entrga:
+
+?       • Evidencias de un tablero para organizar el proyecto con Trello
+todo    • Evidencias del control de versiones en GitHub
+?       • Carpeta del sitio web
+?       • Carpeta de archivos multimedia originales y optimizados
+!       • Guía de estilos
+
+!           *Elementos típicos de una guía de estilos:
+
+!           *Tipografía: especifica qué tipos de letra se deben usar y en qué contexto.
+
+!           *Colores: define una paleta de colores oficial.
+
+!           *Logotipos: normas para el uso adecuado del logotipo, incluyendo variaciones y tamaños.
+
+!           *Tono y voz: directrices sobre cómo debe sonar la comunicación escrita.
+
+!           *Formatos y espaciados: reglas sobre márgenes, interlineados y distribución de elementos en la página.
+
+        • Memoria en PDF con los apartados correspondientes de cada parte incluyendo:
+            ◦ Enlaces a webgrafía de páginas web consultadas y recursos utilizados.
+            ◦ Incidencias, conclusiones…
+            * (Seguir las normas establecidas para la presentación de documentos.)
+        • Exposición ante el grupo en clase. 
+
+
+    Web personalizada:
+
+?       • Web realizada con Bootstrap de temática, contenidos y diseño libres.
+todo    • Rejilla responsive para diferentes dispositivos. 
+?       • Diferentes componentes de bootstrap. 
+?       • Archivo .css
+            *(Aplica preferentemente las clase de boostrap siempre que sea posible.)
+!       • Sass: archivo custom.css reducido 
+
+    Contenido multimedia:
+
+todo    Añade a tu sitio web responsive contenido multimedia optimizado* para la web (formatos, tamaños, calidad....): 
+?           Imágenes
+?           Vídeos
+?           WWWW
+?           Favicon
+?           Carrusel personalizado
+?           Animación
+!           Canvas
+!           Reproductores
+        *(Aplica procesos de optimización a imágenes y vídeos)
+        *(Justifica la importancia de optimizar el contenido multimedia )
+        *(Justifica el uso de los diferentes formatos usados)
+
+    Accesibilidad y usabilidad:
+
+        Aplica los principios de accesibilidad, comenta las técnicas utilizadas.
+        Comprueba sobre tu web los desafíos de accesibilidad que se plantean en el apartado 8. 
+        Realiza sobre tu web los ejercicio planteados en el tema de accesibilidad (apartado 2 actividades 2.2 a 2.6) para la de evaluación, análisis y testeo de accesibilidad web. 0,5
+        Usa herramientas que te ayuden a determinar si el contenido de tu web cumple con los estándares de accesibilidad.
+        Analiza la usabilidad de tu web según el apartado 5.2. Análisis de un sitio web bien diseñado
+        
+    Tablas de rúbrica
+        
+        Ha reconocido las implicaciones de las licencias y los derechos de autor en el uso de material multimedia.
+
+        Ha identificado los formatos de imagen, audio y vídeo a utilizar.
+
+        Ha analizado las herramientas disponibles para generar contenido multimedia.
+
+        Ha empleado herramientas para el tratamiento digital de la imagen.
+
+        Ha utilizado herramientas para manipular audio y vídeo.
+
+        Ha realizado animaciones a partir de imágenes fijas.
+
+        Ha importado y exportado imágenes, audio y vídeo en diversos formatos según su finalidad.
+
+        Ha aplicado la guía de estilo.
+
+        Ha reconocido y analizado las tecnologías relacionadas con la inclusión de contenido multimedia e interactivo.
+
+        Ha identificado las necesidades específicas de configuración de los navegadores Web para soportar contenido multimedia e interactivo.
+
+        Ha utilizado herramientas gráficas para el desarrollo de contenido multimedia e interactivo.
+
+        Ha analizado el código generado por las herramientas de desarrollo de contenido interactivo.
+
+        Ha agregado elementos multimedia a documentos Web.
+
+        Ha añadido interactividad a elementos de un documento Web.
+
+        Ha verificado el funcionamiento de los elementos multimedia e interactivos en distintos navegadores
+
+        Ha reconocido la necesidad de diseñar Webs accesibles.
+
+        Ha analizado la accesibilidad de diferentes documentos Web.
+
+        Ha identificado las principales pautas de accesibilidad al contenido.
+
+        Ha analizado los posibles errores según los puntos de verificación de prioridad.
+
+        Ha alcanzado el nivel de conformidad deseado.
+
+        Ha verificado los niveles alcanzados mediante el uso de test externos.
+
+        Ha verificado la visualización del interfaz con diferentes navegadores y tecnologías.
+
+        Ha analizado la usabilidad de diferentes documentos Web.
+
+        Ha modificado el interfaz Web para adecuarlo al objetivo que persigue y a los usuarios a los que va dirigido
+
+        Ha valorado la importancia del uso de estándares en la creación de documentos Web
+
+        Ha verificado la facilidad de navegación de un documento Web mediante distintos periféricos.
+
+        Ha analizado diferentes técnicas para verificar la usabilidad de un documento Web.
+
+        Ha verificado la usabilidad del interfaz Web creado en diferentes navegadores y tecnologías.
+
+"""
